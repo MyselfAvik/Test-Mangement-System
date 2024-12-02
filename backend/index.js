a2208262cee162e707ff0a373f1ea2cb2aa9d165
Avik Dangar <123013809+MyselfAvik@users.noreply.github.com>
2024-12-03 00:09:18 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
-
+import bodyParser from 'body-parser';
 import "./Models/db.js"
+import cors from "cors"
 const app=express();
 
 const port=process.env.PORT || 8010
-
+app.use(bodyParser.json())
+app.use(cors())
 app.listen(port,()=>{
     console.log(`server is running on ${port}`)
 })
